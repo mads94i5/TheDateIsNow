b48f579cfb157b4d47f40118bae787e65fed33fe
mads94i5 <mads94i5@stud.kea.dk>
2022-03-08 20:11:25 +0100

    Formatting

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,23 +13,15 @@ public class Main {
         System.out.println(theDateIsNow(today.getDayOfWeek().getValue(), today.getDayOfMonth(), today.getMonth().getValue(), today.getYear()));
     }
     private String theDateIsNow(int weekday, int date, int month, int year) {
-        String returnString = "The date is now: ";
+        String returnString = "Today is: ";
         String[] weekdays = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
         String[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
 
         returnString += weekdays[weekday-1] + " ";
-        if (date % 10 == 1) {
-            returnString += "the " + date + "st of ";
-        }
-        else if (date % 10 == 2) {
-            returnString += "the " + date + "nd of ";
-        }
-        else if (date % 10 == 3) {
-            returnString += "the " + date + "rd of ";
-        }
-        else {
-            returnString += "the " + date + "th of ";
-        }
+        if (date % 10 == 1) { returnString += "the " + date + "st of "; }
+        else if (date % 10 == 2) { returnString += "the " + date + "nd of "; }
+        else if (date % 10 == 3) { returnString += "the " + date + "rd of "; }
+        else { returnString += "the " + date + "th of "; }
         returnString += months[month-1] + " ";
         returnString += year + ".";
         return returnString;
